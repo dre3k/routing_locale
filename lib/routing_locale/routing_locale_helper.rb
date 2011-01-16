1d2650034996a5acadaf82234c0d69449a13dbad
Andrei Kulakov <krugliy@gmail.com>
2011-01-16 20:29:57 +0200

    hack for root route

--- lib/routing_locale/routing_locale_helper.rb
+++ lib/routing_locale/routing_locale_helper.rb
@@ -2,7 +2,7 @@ module RoutingLocaleHelper
   POSTFIXES = %w[_path _url]
 
   EXPLICIT_LOCALE_ROUTES = Rails.application.routes.routes.
-    reject{|r| (r.path =~ %r[/rails/info/properties]) || r.name.nil?}.
+    reject{|r| (r.path =~ %r[/rails/info/properties]) || r.name == 'locale_root' || r.name.nil?}.
     map{|r| r.name.to_s}.reject{|r| r !~ /locale_/}
 
   IMPLICIT_LOCALE_ROUTES = EXPLICIT_LOCALE_ROUTES.map{|r| r.sub('locale_', '')}
@@ -10,6 +10,16 @@ module RoutingLocaleHelper
   def self.included(base)
     base.instance_eval do
       POSTFIXES.each do |postfix|
+        #special hack for root route: '/' on default locale
+        define_method ('root' + postfix).to_sym do |*args|
+          if (defined? params[:locale]) && params[:locale]
+            send ('locale_root' + postfix).to_sym, params[:locale], *args
+          else
+            '/'
+          end
+        end
+
+        # all other locale routes
         EXPLICIT_LOCALE_ROUTES.each_with_index do |explicit_route, i|
           implicit_route = IMPLICIT_LOCALE_ROUTES[i]
 
