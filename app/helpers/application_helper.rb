e0309bc3e531e9060651e574a87c062b7594d72d
Andrei Kulakov <krugliy@gmail.com>
2011-01-16 17:34:12 +0200

    remove routing_locale_helpers and include RoutingLocaleHelper in application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,29 +1,5 @@
-def routing_locale_helpers(*resources)
-  postfixes = %w[_path _url]
-  prefixes  = %w[edit_ new_] << ''
-  resources.each do |resource|
-    singular_name = resource.to_s
-    plural_name = resource.to_s.pluralize
-    postfixes.each do |postfix|
-      self.class_eval do
-        define_method (name = plural_name + postfix).to_sym do |*args|
-          locale = (defined? params[:locale]) && (locale = params[:locale]) ? locale : nil
-          send(("locale_" + name).to_sym , locale, *args)
-        end
-      end
-
-      prefixes.each do |prefix|
-        self.class_eval do
-          define_method (prefix + name = (singular_name + postfix)).to_sym do |*args|
-            locale = (defined? params[:locale]) && (locale = params[:locale]) ? locale : nil
-            send((prefix + "locale_" + name).to_sym , locale, *args)
-          end
-        end
-      end
-    end
-  end
-end
+require 'routing_locale/routing_locale_helper'
 
 module ApplicationHelper
-  routing_locale_helpers :post
+  include RoutingLocaleHelper
 end
